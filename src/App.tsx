7ae2a0cfaf88f52ec2c02ebace4d59be21a3259c
Nicolas Van Eenaeme <nicolas@poison.be>
2024-08-07 23:46:35 +0200

    :sparkles: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>StarApps Countdown</h1>
+      <div className="card">
+        The timer component goes here
+      </div>
+    </>
+  )
+}
+
+export default App
